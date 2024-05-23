13f7e03d7feeabde056d632af0923a85e7529c99
Mizuki <mbaba@kanolab.net>
2024-05-24 00:01:16 +0900

    chore: 適切なeslint設定の追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "eslint-config-prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2021,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["./", "../"],
+      },
+    ],
+  },
 };
